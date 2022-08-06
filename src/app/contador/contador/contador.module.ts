c409a35e2aae0380651f7950c59a9dbaec0c86b0
madelen-illanes <madelen2811@gmail.com>
2022-08-06 12:13:34 -0500

    primer commit

---
+++ src/app/contador/contador/contador.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { ContadorComponent } from './contador.componente';
+
+@NgModule({
+    declarations: [
+        ContadorComponent
+    ],
+    imports: [ ],
+    exports: [ContadorComponent],
+    providers: [],
+})
+export class ContadorModule {} 
